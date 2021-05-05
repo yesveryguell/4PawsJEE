dec7c870b4fab73dd0279c2378680788891d9cd7
Felipe Rojas <jfrojasr@unbosque.edu.co>
2021-05-05 17:22:50 -0500

    some files

---
+++ src/main/java/edu/unbosque/persistence/Archivo.java
@@ -0,0 +1,76 @@
+package edu.unbosque.persistence;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Archivo {
+
+	private ObjectInputStream entrada;
+	private ObjectOutputStream salida;
+
+	public Archivo() {
+
+	}
+
+	public Archivo(File archivo) {
+
+		if (archivo.exists()) {
+
+		} else {
+			try {
+				archivo.createNewFile();
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void escribirEnArchivo(ArrayList<Duenio> alDuenio, File archivo) {
+
+		try {
+
+			salida = new ObjectOutputStream(new FileOutputStream(archivo));
+			salida.writeObject(alDuenio);
+			salida.close();
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	public ArrayList<Duenio> leerArchivo(File archivo) {
+
+		ArrayList<Duenio> alDuenio = new ArrayList<Duenio>();
+
+		if (archivo.length() != 0)
+
+		{
+			try {
+
+				entrada = new ObjectInputStream(new FileInputStream(archivo));
+				alDuenio = (ArrayList<Duenio>) entrada.readObject();
+
+			} catch (FileNotFoundException e) {
+
+				e.printStackTrace();
+
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+
+				e.printStackTrace();
+			}
+		}
+
+		return alDuenio;
+	}
+}
